3d3407aef4fe98345390b7784cf39116dfc318f8
henrique-ol-dev <riqueneguin467@gmail.com>
2020-10-01 02:57:17 -0300

    Criptografia

---
+++ Nyous/Contexts/NyousContext.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Nyous.Domains;
+
+namespace Nyous.Contexts
+{
+    public partial class NyousContext : DbContext
+    {
+        public NyousContext()
+        {
+        }
+
+        public NyousContext(DbContextOptions<NyousContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Acesso> Acesso { get; set; }
+        public virtual DbSet<Categoria> Categoria { get; set; }
+        public virtual DbSet<Convite> Convite { get; set; }
+        public virtual DbSet<Evento> Evento { get; set; }
+        public virtual DbSet<Localizacao> Localizacao { get; set; }
+        public virtual DbSet<Presença> Presença { get; set; }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-RAOH29V; Initial Catalog= Nyous; Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Acesso>(entity =>
+            {
+                entity.HasKey(e => e.IdAcesso)
+                    .HasName("PK__Acesso__CDF01DA1850C41D8");
+
+                entity.Property(e => e.Tipo)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoria)
+                    .HasName("PK__Categori__A3C02A10F0C1FE78");
+
+                entity.Property(e => e.Titulo)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Convite>(entity =>
+            {
+                entity.HasKey(e => e.IdConvite)
+                    .HasName("PK__Convite__318FC5546A10C8EE");
+
+                entity.HasOne(d => d.IdEventoNavigation)
+                    .WithMany(p => p.Convite)
+                    .HasForeignKey(d => d.IdEvento)
+                    .HasConstraintName("FK__Convite__IdEvent__4CA06362");
+
+                entity.HasOne(d => d.IdUsuarioConvidadoNavigation)
+                    .WithMany(p => p.ConviteIdUsuarioConvidadoNavigation)
+                    .HasForeignKey(d => d.IdUsuarioConvidado)
+                    .HasConstraintName("FK__Convite__IdUsuar__4E88ABD4");
+
+                entity.HasOne(d => d.IdUsuarioEmissorNavigation)
+                    .WithMany(p => p.ConviteIdUsuarioEmissorNavigation)
+                    .HasForeignKey(d => d.IdUsuarioEmissor)
+                    .HasConstraintName("FK__Convite__IdUsuar__4D94879B");
+            });
+
+            modelBuilder.Entity<Evento>(entity =>
+            {
+                entity.HasKey(e => e.IdEvento)
+                    .HasName("PK__Evento__034EFC049F45D5C3");
+
+                entity.Property(e => e.AcessoRestrito)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsFixedLength()
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.DataEvento).HasColumnType("datetime");
+
+                entity.Property(e => e.Descricao)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdCategoriaNavigation)
+                    .WithMany(p => p.Evento)
+                    .HasForeignKey(d => d.IdCategoria)
+                    .HasConstraintName("FK__Evento__IdCatego__44FF419A");
+
+                entity.HasOne(d => d.IdLocalizacaoNavigation)
+                    .WithMany(p => p.Evento)
+                    .HasForeignKey(d => d.IdLocalizacao)
+                    .HasConstraintName("FK__Evento__IdLocali__440B1D61");
+            });
+
+            modelBuilder.Entity<Localizacao>(entity =>
+            {
+                entity.HasKey(e => e.IdLocalizacao)
+                    .HasName("PK__Localiza__C96A5BF62367B16E");
+
+                entity.Property(e => e.Bairro)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cep)
+                    .HasColumnName("CEP")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cidade)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Complemento)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Logradouro)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Numero)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Uf)
+                    .HasColumnName("UF")
+                    .HasMaxLength(2)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+            });
+
+            modelBuilder.Entity<Presença>(entity =>
+            {
+                entity.HasKey(e => e.IdPresenca)
+                    .HasName("PK__Presença__50FB6F5D90D8D10F");
+
+                entity.HasOne(d => d.IdEventoNavigation)
+                    .WithMany(p => p.Presença)
+                    .HasForeignKey(d => d.IdEvento)
+                    .HasConstraintName("FK__Presença__IdEven__47DBAE45");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Presença)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__Presença__IdUsua__48CFD27E");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__5B65BF97CA5952B6");
+
+                entity.Property(e => e.DataNascimento).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(80)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdAcessoNavigation)
+                    .WithMany(p => p.Usuario)
+                    .HasForeignKey(d => d.IdAcesso)
+                    .HasConstraintName("FK__Usuario__IdAcess__2A4B4B5E");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Nyous/Controllers/UsuariosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nyous.Contexts;
+using Nyous.Domains;
+using Nyous.Utils;
+
+namespace Nyous.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private NyousContext _context = new NyousContext();
+
+        public UsuariosController(NyousContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuario()
+        {
+            return await _context.Usuario.ToListAsync();
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // PUT: api/Usuarios/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuario(int id, Usuario usuario)
+        {
+            if (id != usuario.IdUsuario)
+            {
+                return BadRequest();
+            }
+
+            // Criptografamos antes de salvar a senha
+            usuario.Senha = Crypto.Criptografar(usuario.Senha, usuario.Email.Substring(0, 4));
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+
+            // Criptografamos antes de salvar a senha
+            usuario.Senha = Crypto.Criptografar(usuario.Senha, usuario.Email.Substring(0, 4));
+
+            _context.Usuario.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuario", new { id = usuario.IdUsuario }, usuario);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> DeleteUsuario(int id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuario.Any(e => e.IdUsuario == id);
+        }
+    }
+}
